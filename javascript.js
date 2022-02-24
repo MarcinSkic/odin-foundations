76d2c8446e28a99a995ecf6f3c755884f9bfe468
MarcinSkic <marcinskicoficjal@gmail.com>
2022-02-24 21:45:04 +0100

    [js-fundamentals] Frist commit
    Site has welcoming page with link to one page which is
    under construction

---
+++ javascript.js
@@ -0,0 +1,42 @@
+"use strict";
+console.log("Checking pylons");
+let warning = "System compromised!";
+//alert(warning);
+let admin,
+    name;
+name = "John";
+admin = name;
+//alert(admin);
+
+const OUR_PLANET_NAME = "Earth";
+let currentUserName;
+
+function WhatTheF__k() {
+    phrase = "Elo";
+
+    if(false){
+        var phrase;
+    }
+
+    console.log(phrase);
+}
+WhatTheF__k();
+
+//console.log(23+9+3122+30+33+2);
+//console.log((4+6+9)/77);
+
+let a = 10
+console.log(a);
+9*a;
+let b = 7*a;
+console.log(b);
+
+let max = 57;
+let actual = max-13;
+let percentage = actual / max;
+let cos = `Oh no! Only ${"'`' can deal with inserted variables"}`
+console.log(cos);
+
+let str = "Apple, Banana, Kiwi";
+let part = str.slice(-4);
+console.log(part);

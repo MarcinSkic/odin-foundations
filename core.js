ac61402093cbcf894a7ebbcabc1e15a2fd131974
MarcinSkic <marcinskicoficjal@gmail.com>
2022-03-22 11:03:44 +0100

    [etch-a-sketch-odin] Add mode switching with radio button

--- core.js
+++ core.js
@@ -1,21 +1,67 @@
 const container = document.querySelector('.container');
 const generateButton = document.querySelector('#generate');
+const drawingModeRadioButtons = document.querySelectorAll('input[name="mode"]');
+
 
 let containerWidth = 400;
-let squaresInRow = 16;
+let squaresInRow = 64;
 let pencilColor = '#000000';
 let divs = [];
-let mouseDown = false;
+let isMouseDown = false;
+let defaultMode = "hold";
 
 container.style.width = `${containerWidth}px`;
 container.style.height = container.style.width;
 
 generateButton.addEventListener('click',createNewGrid);
 
-document.addEventListener('mousedown',() => mouseDown = true);
-document.addEventListener('mouseup',() => mouseDown = false);
+drawingModeRadioButtons.forEach(radio => {
+    if(radio.value === defaultMode){
+        radio.checked = true;
+        return;
+    }
+});
 
-//document.addEventListener('click',() => mouseDown = !mouseDown);
+drawingModeRadioButtons.forEach(radio => radio.addEventListener('change',assignRadioModeChange));
+
+let holdModeDown = function (){
+    isMouseDown = true;
+};
+
+let holdModeUp = function () {
+    isMouseDown = false;
+};
+
+let clickMode = function () {
+    isMouseDown = !isMouseDown;
+};
+
+function assignRadioModeChange () {
+    if(this.checked){
+        removeDrawingListeners ();
+
+        switchDrawingMode(this.value);
+    }
+}
+
+function switchDrawingMode(mode){
+    if(mode === 'hold'){
+        document.addEventListener('mousedown',holdModeDown);
+        document.addEventListener('mouseup',holdModeUp);
+    } else {
+        document.addEventListener('click',clickMode);
+    }
+    isMouseDown = false;
+}
+
+function removeDrawingListeners (){
+    document.removeEventListener('mousedown',holdModeDown);
+    document.removeEventListener('mouseup',holdModeUp);
+
+    document.removeEventListener('click',clickMode);
+}
+
+switchDrawingMode(defaultMode);
 
 generateDivs();
 
@@ -32,7 +78,7 @@ function generateDivs(){
     for(let i = 0; i < squaresInRow**2; i++){
         const div = document.createElement('div');
 
-        div.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        div.style.backgroundColor = `white`;
         div.style.width = `${containerWidth/squaresInRow}px`
         div.classList.toggle('square');
 
@@ -41,15 +87,15 @@ function generateDivs(){
         div.addEventListener('drop',e => e.preventDefault());
 
         div.addEventListener('mouseenter',colourSquare);
-        div.addEventListener('click',colourSquare);
+        div.addEventListener('mousedown',colourSquare);
         
         container.appendChild(div);
         divs.push(div);
-    }
+    }   //#${Math.floor(Math.random()*16777215).toString(16)}
 }
 
 function colourSquare(event){
-    if(mouseDown || event.type === 'click'){
+    if(isMouseDown || event.type === 'mousedown'){
         event.target.style.backgroundColor = pencilColor;
     }
 }
